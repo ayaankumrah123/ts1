88403d0ceb5e9b2fcda4eb0c303164c9c58f562c
ayaankumrah123 <76741090+ayaankumrah123@users.noreply.github.com>
2021-05-10 13:01:07 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,29 @@
+class slingShot {
+
+    constructor(bodyA,pointB){
+    var options={
+    bodyA:bodyA,
+    pointB:pointB,
+    stiffness:0.4,
+    length:1
+    }
+    // this.bodyA=bodyA,
+    
+    this.sling=Constraint.create(options);
+    this.pointB=pointB;
+    World.add(world,this.sling)
+    }
+    fly() {
+        this.sling.bodyA=null ;
+    }
+    display(){
+     if (this.sling.bodyA) {
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(4);
+        stroke("turquoise");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    }
+}
